068265053bcbda77b16caa2e1dae58f4d4ff20ac
sa1987 <aswathi.tcs@gmail.com>
2020-08-20 22:30:22 +0100

    dq
    dq

--- InvestigatingCOVID-19VirusTrends.R
+++ InvestigatingCOVID-19VirusTrends.R
@@ -15,4 +15,15 @@ glimpse(vector_cols)
 
 covid_df_all_states <- filter(covid_df, Province_State == "All States")
 
-covid_df_all_states_daily
+covid_df_all_states_daily <- select(covid_df_all_states, Date, Country_Region, active, hospitalizedCurr, daily_tested, daily_positive)
+
+covid_df_all_states_daily_sum <- covid_df_all_states_daily %>% group_by(Country_Region) %>%
+  summarise(tested = sum(daily_tested),
+            positive = sum(daily_positive),
+            active = sum (active),
+            hospitalized = sum(hospitalizedCurr)) %>%
+  arrange(-tested)
+
+print(covid_df_all_states_daily_sum)
+
+covid_top_10 <- head(covid_df_all_states_daily_sum, 10)
